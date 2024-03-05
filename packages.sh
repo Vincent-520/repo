5e7bd94730bf26cb9dddbb86835f7db0ef76e6d7
Vincent-520 <hyp18645351532@gmail.com>
2024-03-06 03:45:41 +0800

    Sync Depths

--- packages.sh
+++ packages.sh
@@ -6,5 +6,4 @@ xz -c9 Packages > Packages.xz
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 lzma -c9 Packages > Packages.lzma
-zstd -q -c19 Packages > Packages.zst
 find ./ -name ".DS_Store" -depth -exec rm {} \;
